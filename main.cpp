72fa01e07b7e6f40d6fd72049e174427293e4883
CaioRS <caiorss.rodrigues@gmail.com>
2019-10-13 00:56:20 -0300

    Refactoring - Move code from the application launcher tab in AppMainWindow to new class Tab_ApplicationLauncher.

--- main.cpp
+++ main.cpp
@@ -350,23 +350,147 @@ public:
 }; //----- End of class DesktopBookmarksTable ---------//
 
 
-class AppMainWindow: public QMainWindow
+class Tab_ApplicationLauncher
 {
-private:
-    FormLoader   loader;
-    QWidget*     form;
+    FormLoader* loader;
+    QWidget*    parent;
+
     // Extract children widgets from from file
     QLineEdit*   cmd_input;
     QCheckBox*   chb_editable;
     QCheckBox*   chb_always_on_top;
     QListWidget* app_registry;
+public:
 
+    Tab_ApplicationLauncher(QWidget* parent, FormLoader* loader):
+        parent(parent), loader{loader}
+    {
+        //========= Tab - Application Launcher ==============///
+
+        // Load controls named in the form "user_interface.ui"
+        cmd_input         = loader->find_child<QLineEdit>("cmd_input");
+        app_registry      = loader->find_child<QListWidget>("cmd_registry");
+        chb_editable      = loader->find_child<QCheckBox>("chb_editable");
+        chb_always_on_top = loader->find_child<QCheckBox>("chb_always_on_top");
+
+
+        // See: https://www.qtcentre.org/threads/15464-WindowStaysOnTopHint
+        loader->on_clicked<QCheckBox>("chb_always_on_top",
+                                     [&self = *this]
+                                     {
+#if 1
+                                         QMessageBox::warning( self.parent
+                                                              , "Error report"
+                                                              , "Functionality not implemented yet."
+                                                              );
+#endif
+                                         // static auto flags = self.windowFlags();
+                                         // flags ^=  Qt::WindowStaysOnTopHint;
+                                         // self.show();
+                                         // self.activateWindow();
+                                     });
+
+        loader->on_button_clicked("btn_add", [&self = *this]
+                                 {
+                                     auto text = self.cmd_input->text();
+                                     if(text.isEmpty()) { return; }
+                                     auto item = new QListWidgetItem();
+                                     item->setText(text);
+                                     self.app_registry->insertItem(0, item);
+                                     self.cmd_input->clear();
+                                     self.save_settings();
+                                 });
+        // qtutils::on_clicked(btn_add,);
+
+        // Signals and slots with member function pointer
+        // QObject::connect(btn_remove, &QPushButton::clicked, this, &CustomerForm::Reset);
+
+        // Signals and slots with lambda function
+        loader->on_button_clicked("btn_run", [self = this]{ self->run_selected_item(); });
+
+
+        // Launch application double clicked application from registry (QListWidget)
+        qtutils::on_double_clicked(app_registry, [&self = *this]
+                                   {
+                                       if(self.chb_editable->isChecked())
+                                       {
+                                           auto item = self.app_registry->currentItem();
+                                           if(item == nullptr) { return; }
+                                           // Set item as editable
+                                           item->setFlags( item->flags() | Qt::ItemIsEditable);
+                                           self.save_settings();
+                                           return;
+                                       }
+                                       self.run_selected_item();
+                                       // auto command = items.first()->text();
+                                   });
+
+        loader->on_button_clicked("btn_remove",
+                                 [&self = *this]
+                                 {
+                                     QListWidgetItem* pItem = self.app_registry->currentItem();
+                                     if(pItem == nullptr) { return; }
+                                     self.app_registry->removeItemWidget(pItem);
+                                     delete pItem;
+                                     self.save_settings();
+                                 });
+
+    } // --- End of Tab_ApplicationLauncher CTOR -------//
+
+    // Run item selected in the QListWidget (ApplicationRegistry)
+    void run_selected_item()
+    {
+        auto& self = *this;
+        auto items = self.app_registry->selectedItems();
+        if(items.isEmpty()) { return; }
+        auto command = items.first()->text();
+
+        bool status = QProcess::startDetached(command);
+
+        std::cout << " [INFO] Run command " << command.toStdString()
+                  << " status = " << (status ? "OK" : "FAILURE")
+                  << std::endl;
+    }
+
+    /// Add new command to command registry widget
+    void add_item(QString command)
+    {
+        this->app_registry->addItem(command);
+    }
+
+    /// Return number of elements in the command registry list widget
+    int count()
+    {
+        return this->app_registry->count();
+    }
+
+    /// Return pointer to element at nth row
+    QListWidgetItem*
+    at(int row)
+    {
+        return this->app_registry->item(row);
+    }
+
+    void save_settings()
+    {
+
+    }
+
+
+}; // ---- End of class Tab_ApplicationLauncher ----------//
+
+
+class AppMainWindow: public QMainWindow
+{
+private:
+    FormLoader   loader;
+    QWidget*     form;
 
     //======== TrayIcon =============================//
     QSystemTrayIcon* tray_icon;
 
-
-    std::unique_ptr<Tab_DesktopBookmarks> tab_deskbookmarks;
+    std::unique_ptr<Tab_DesktopBookmarks>    tab_deskbookmarks;
+    std::unique_ptr<Tab_ApplicationLauncher> tab_applauncher;
 
 public:
 
@@ -376,14 +500,7 @@ public:
     {
         form = loader.GetForm();
         tab_deskbookmarks = std::make_unique<Tab_DesktopBookmarks>(this, &loader);
-
-        //========= Tab - Application Launcher ==============///
-
-        // Load controls named in the form "user_interface.ui"
-        cmd_input         = loader.find_child<QLineEdit>("cmd_input");
-        app_registry      = loader.find_child<QListWidget>("cmd_registry");
-        chb_editable      = loader.find_child<QCheckBox>("chb_editable");
-        chb_always_on_top = loader.find_child<QCheckBox>("chb_always_on_top");
+        tab_applauncher   = std::make_unique<Tab_ApplicationLauncher>(this, &loader);
 
         //========= Create Tray Icon =======================//
 
@@ -425,66 +542,6 @@ public:
         // Enable Drag and Drop Event
         this->setAcceptDrops(true);
 
-        // See: https://www.qtcentre.org/threads/15464-WindowStaysOnTopHint
-        loader.on_clicked<QCheckBox>("chb_always_on_top",
-                                     [&self = *this]
-                                     {
-                                #if 1
-                                         QMessageBox::warning( &self
-                                                              , "Error report"
-                                                              , "Functionality not implemented yet."
-                                                              );
-                                #endif
-                                // static auto flags = self.windowFlags();
-                                // flags ^=  Qt::WindowStaysOnTopHint;
-                                // self.show();
-                                // self.activateWindow();
-                                 });
-
-        loader.on_button_clicked("btn_add", [&self = *this]
-                                 {
-                                     auto text = self.cmd_input->text();
-                                     if(text.isEmpty()) { return; }
-                                     auto item = new QListWidgetItem();
-                                     item->setText(text);
-                                     self.app_registry->insertItem(0, item);
-                                     self.cmd_input->clear();
-                                     self.save_settings();
-                                 });
-        // qtutils::on_clicked(btn_add,);
-
-        // Signals and slots with member function pointer
-        // QObject::connect(btn_remove, &QPushButton::clicked, this, &CustomerForm::Reset);
-
-        // Signals and slots with lambda function
-        loader.on_button_clicked("btn_run", [self = this]{ self->run_selected_item(); });
-
-
-        // Launch application double clicked application from registry (QListWidget)
-        qtutils::on_double_clicked(app_registry, [&self = *this]
-                                   {
-                                       if(self.chb_editable->isChecked())
-                                       {
-                                           auto item = self.app_registry->currentItem();
-                                           if(item == nullptr) { return; }
-                                           // Set item as editable
-                                           item->setFlags( item->flags() | Qt::ItemIsEditable);
-                                           self.save_settings();
-                                           return;
-                                       }
-                                       self.run_selected_item();
-                                       // auto command = items.first()->text();
-                                   });
-
-        loader.on_button_clicked("btn_remove",
-                                 [&self = *this]
-                                 {
-                                     QListWidgetItem* pItem = self.app_registry->currentItem();
-                                     if(pItem == nullptr) { return; }
-                                     self.app_registry->removeItemWidget(pItem);
-                                     delete pItem;
-                                     self.save_settings();
-                                 });
 
 
         // Save application state when the main Window is destroyed
@@ -497,24 +554,11 @@ public:
 
     } // --- End of CustomerForm ctor ------//
 
-    // Run item selected in the QListWidget (ApplicationRegistry)
-    void run_selected_item()
-    {
-        auto& self = *this;
-        auto items = self.app_registry->selectedItems();
-        if(items.isEmpty()) { return; }
-        auto command = items.first()->text();
-
-        bool status = QProcess::startDetached(command);
-
-        std::cout << " [INFO] Run command " << command.toStdString()
-                  << " status = " << (status ? "OK" : "FAILURE")
-                  << std::endl;
-    }
-
+#if 0
     // See: https://stackoverflow.com/questions/18934964
     bool eventFilter(QObject* object, QEvent* event) override
     {
+
         if(object == this->app_registry && event->type() == QEvent::KeyRelease)
         {
             QKeyEvent* ke = static_cast<QKeyEvent*>(event);
@@ -526,6 +570,7 @@ public:
         }
         return false;
     }
+#endif
 
     /// Make this window stay alwys on top
     void setWindowAlwaysOnTop()
@@ -596,7 +641,8 @@ public:
         auto settings = QSettings(settings_file, QSettings::IniFormat);
         auto commands = settings.value("commands/list").toStringList();
         for(auto const& cmd: commands){
-            this->app_registry->addItem(cmd);
+            this->tab_applauncher->add_item(cmd);
+            //this->app_registry->addItem(cmd);
         }
 
         auto files_bookmarks = settings.value("files_bookmarks/list")
@@ -617,9 +663,9 @@ public:
         auto settings_file = this->get_settings_file();
         auto settings = QSettings(settings_file, QSettings::IniFormat);
         QStringList list;
-        for(int i = 0; i < this->app_registry->count(); i++)
+        for(int i = 0; i < this->tab_applauncher->count(); i++)
         {
-            QListWidgetItem* item = this->app_registry->item(i);
+            QListWidgetItem* item = this->tab_applauncher->at(i);
             list << item->text();
         }
         settings.setValue("commands/list", list);
