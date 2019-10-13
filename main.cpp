8aae7b07dec3536e1088ad0ae198af39be999ae8
CaioRS <caiorss.rodrigues@gmail.com>
2019-10-13 01:14:09 -0300

    Move class Tab_ApplicationLauncher to its own files: tab_applicationlauncher.hpp and tab_applicationlauncher.cpp.

--- main.cpp
+++ main.cpp
@@ -18,6 +18,7 @@
 
 #include "FormLoader.hpp"
 #include "RecordTableModel.hpp"
+#include "tab_applicationlauncher.hpp"
 
 namespace qtutils
 {
@@ -350,136 +351,6 @@ public:
 }; //----- End of class DesktopBookmarksTable ---------//
 
 
-class Tab_ApplicationLauncher
-{
-    FormLoader* loader;
-    QWidget*    parent;
-
-    // Extract children widgets from from file
-    QLineEdit*   cmd_input;
-    QCheckBox*   chb_editable;
-    QCheckBox*   chb_always_on_top;
-    QListWidget* app_registry;
-public:
-
-    Tab_ApplicationLauncher(QWidget* parent, FormLoader* loader):
-        parent(parent), loader{loader}
-    {
-        //========= Tab - Application Launcher ==============///
-
-        // Load controls named in the form "user_interface.ui"
-        cmd_input         = loader->find_child<QLineEdit>("cmd_input");
-        app_registry      = loader->find_child<QListWidget>("cmd_registry");
-        chb_editable      = loader->find_child<QCheckBox>("chb_editable");
-        chb_always_on_top = loader->find_child<QCheckBox>("chb_always_on_top");
-
-
-        // See: https://www.qtcentre.org/threads/15464-WindowStaysOnTopHint
-        loader->on_clicked<QCheckBox>("chb_always_on_top",
-                                     [&self = *this]
-                                     {
-#if 1
-                                         QMessageBox::warning( self.parent
-                                                              , "Error report"
-                                                              , "Functionality not implemented yet."
-                                                              );
-#endif
-                                         // static auto flags = self.windowFlags();
-                                         // flags ^=  Qt::WindowStaysOnTopHint;
-                                         // self.show();
-                                         // self.activateWindow();
-                                     });
-
-        loader->on_button_clicked("btn_add", [&self = *this]
-                                 {
-                                     auto text = self.cmd_input->text();
-                                     if(text.isEmpty()) { return; }
-                                     auto item = new QListWidgetItem();
-                                     item->setText(text);
-                                     self.app_registry->insertItem(0, item);
-                                     self.cmd_input->clear();
-                                     self.save_settings();
-                                 });
-        // qtutils::on_clicked(btn_add,);
-
-        // Signals and slots with member function pointer
-        // QObject::connect(btn_remove, &QPushButton::clicked, this, &CustomerForm::Reset);
-
-        // Signals and slots with lambda function
-        loader->on_button_clicked("btn_run", [self = this]{ self->run_selected_item(); });
-
-
-        // Launch application double clicked application from registry (QListWidget)
-        qtutils::on_double_clicked(app_registry, [&self = *this]
-                                   {
-                                       if(self.chb_editable->isChecked())
-                                       {
-                                           auto item = self.app_registry->currentItem();
-                                           if(item == nullptr) { return; }
-                                           // Set item as editable
-                                           item->setFlags( item->flags() | Qt::ItemIsEditable);
-                                           self.save_settings();
-                                           return;
-                                       }
-                                       self.run_selected_item();
-                                       // auto command = items.first()->text();
-                                   });
-
-        loader->on_button_clicked("btn_remove",
-                                 [&self = *this]
-                                 {
-                                     QListWidgetItem* pItem = self.app_registry->currentItem();
-                                     if(pItem == nullptr) { return; }
-                                     self.app_registry->removeItemWidget(pItem);
-                                     delete pItem;
-                                     self.save_settings();
-                                 });
-
-    } // --- End of Tab_ApplicationLauncher CTOR -------//
-
-    // Run item selected in the QListWidget (ApplicationRegistry)
-    void run_selected_item()
-    {
-        auto& self = *this;
-        auto items = self.app_registry->selectedItems();
-        if(items.isEmpty()) { return; }
-        auto command = items.first()->text();
-
-        bool status = QProcess::startDetached(command);
-
-        std::cout << " [INFO] Run command " << command.toStdString()
-                  << " status = " << (status ? "OK" : "FAILURE")
-                  << std::endl;
-    }
-
-    /// Add new command to command registry widget
-    void add_item(QString command)
-    {
-        this->app_registry->addItem(command);
-    }
-
-    /// Return number of elements in the command registry list widget
-    int count()
-    {
-        return this->app_registry->count();
-    }
-
-    /// Return pointer to element at nth row
-    QListWidgetItem*
-    at(int row)
-    {
-        return this->app_registry->item(row);
-    }
-
-    void save_settings()
-    {
-
-    }
-
-
-}; // ---- End of class Tab_ApplicationLauncher ----------//
-
-
 class AppMainWindow: public QMainWindow
 {
 private:
@@ -499,8 +370,8 @@ public:
         : loader{FormLoader(this, ":/assets/user_interface.ui")}
     {
         form = loader.GetForm();
-        tab_deskbookmarks = std::make_unique<Tab_DesktopBookmarks>(this, &loader);
         tab_applauncher   = std::make_unique<Tab_ApplicationLauncher>(this, &loader);
+        tab_deskbookmarks = std::make_unique<Tab_DesktopBookmarks>(this, &loader);
 
         //========= Create Tray Icon =======================//
 
