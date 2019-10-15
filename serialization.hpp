4452a67e42c62b05609b499c2b4028d174e732d6
CaioRS <caiorss.rodrigues@gmail.com>
2019-10-15 16:33:33 -0300

    Refactor namespace qtutils::serialization - create extension points through template specialization.

--- serialization.hpp
+++ serialization.hpp
@@ -6,11 +6,59 @@
 
 //--- QT Headers ---//
 #include <QtCore>
-
+#include <QtWidgets>
 
 
 namespace qtutils::serialization
 {
+
+template<typename T>
+QVariant value_writer(T& value)
+{
+    return value;
+}
+
+template<typename T>
+QVariant value_writer(QList<T>& value)
+{
+    QByteArray arr;
+    QDataStream ss{&arr, QIODevice::WriteOnly};
+    ss << value;
+    return arr;
+}
+
+template<typename T>
+inline void value_reader(T& ref, QVariant value);
+
+template<>
+inline void value_reader(int& ref, QVariant value)
+{
+    ref = value.toInt();
+}
+
+template<>
+inline void value_reader(double& ref, QVariant value)
+{
+    ref = value.toDouble();
+}
+
+template<>
+inline void value_reader(QString& ref, QVariant value)
+{
+    ref = value.toString();
+}
+
+template<>
+inline void value_reader(QVariant& ref, QVariant value)
+{
+    ref = value;
+}
+
+template<>
+inline void value_reader(QStringList& ref, QVariant value)
+{
+    ref = value.toStringList();
+}
 
 
 // Note 1: The QDatStream object is not owned by this class
@@ -51,6 +99,14 @@ public:
     template<typename T>
     void visit(QString name, T& value)
     {
+        dataset[name] = value_writer<T>(value);
+    }
+
+
+#if 0
+    template<typename T>
+    void visit(QString name, T& value)
+    {
         dataset[name] = value;
     }
 
@@ -62,6 +118,8 @@ public:
         ss << value;
         dataset[name] = arr;
     }
+#endif
+
 };
 
 
@@ -111,13 +169,21 @@ public:
     }
 
     template<typename Visitable>
-    void operator()(Visitable&& visitable)
+    void operator()(Visitable& visitable)
     {
         (*pss) >> dataset;
         visitable.accept(*this);
         dataset.clear();
     }
 
+    template<typename T>
+    void visit(QString name, T& ref)
+    {
+        QVariant x = dataset[name];
+        value_reader(ref, x);
+    }
+
+#if 0
     void visit(QString name, int& value)
     {
         value = dataset[name].toInt();
@@ -139,6 +205,7 @@ public:
     {
         value = dataset[name].toString();
     }
+#endif
 
     template<typename T>
     void visit(QString name, QList<T>& value)
